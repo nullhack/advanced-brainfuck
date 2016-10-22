dfa3652311beafabb795a0b05f90ea003459f5cc
Eric Lopes <eric.oliveira.lopes@gmail.com>
2016-10-22 09:13:14 +0000

    Adding max recursion feature and simplified import

--- brainfuck.py
+++ brainfuck.py
@@ -1,3 +1,4 @@
+import os
 import re
 import sys
 import argparse
@@ -12,19 +13,28 @@ class BrainFuck:
         self._jump = {}
 
     def _print_value(self):
-        print(self.cells[self.pointer])
+        if not self.cells[self.pointer]:
+            print()
+        else:
+            try:
+                print(chr(self.cells[self.pointer]), end="")
+            except:
+                print(self.cells[self.pointer], end="")
 
     def _read_value(self):
         while True:
+            ui = input('<< ')
             try:
-                ui = input('<< ')
-                if not ui: break
                 self.cells[self.pointer] = int(ui)
-            except:
-                print("Invalid integer!")
-                continue
-            else:
                 break
+            except:
+                pass
+
+            try:
+                self.cells[self.pointer] = ord(ui)
+                break
+            except:
+                print("Invalid value! Please try again:")
 
     def _move_right(self):
         self.pointer += 1
@@ -69,19 +79,23 @@ class BrainFuck:
         import_list = re.findall('{([a-zA-Z0-9_\-\/]+)\}', cmd_line)
         import_dict = {}
         for lib in import_list:
-            try:
-                with open('{}.bf'.format(lib)) as flib:
-                    bf = BrainFuck()
-                    istr = ''.join(flib.readlines())
-                    bf.execute(istr)
-                    import_dict[lib] = bf.cmd_history
+            lib_path = os.path.join(
+                os.path.dirname(__file__),
+                'bflib', '{}.bf'.format(lib))
+            if not os.path.isfile(lib_path):
+                lib_path = '{}.bf'.format(lib)
+                if not os.path.isfile(lib_path):
+                    raise Exception(
+                        'Could not import: {}'.format(lib_path))
+            with open(lib_path) as flib:
                 print('importing:', lib)
-            except:
-                import_dict[lib] = ''
-                print('Could not import: ', lib)
+                bf = BrainFuck()
+                istr = ''.join(flib.readlines())
+                bf.execute(istr)
+                import_dict[lib] = bf.cmd_history
         return import_dict
 
-    def execute(self, cmd_line):
+    def execute(self, cmd_line, MAX_RECURSION=10**4):
         if not self.is_balanced(cmd_line):
             raise Exception("brackets not balanced!")
         cmds = {
@@ -94,21 +108,27 @@ class BrainFuck:
             '-':self._sub,
             '[':self._open_brackets,
             ']':self._close_brackets,
-
             #Additional commands
             '*':self.print_cells,
             '&':self.print_cmd_history,
             }
-        pos = len(self.cmd_history)
-        stack = []
-        try:
+
+        backup_history = self.cmd_history
+        backup_cells = self.cells.backup()
+
+        try: 
             import_dict = self.import_lib(cmd_line)
             cmd_line = cmd_line.format(**import_dict)
-        except:
-            print("Lib must be an string without dots, numbers or extensions")
+        except Exception as e:
             cmd_line = ''
-        #clean the input, keeping only valid commands
+            print(e)
+
+        #Clean the input, keeping only valid commands
         cmd_line = [c for c in cmd_line if c in cmds]
+
+        #Prepare jump table
+        pos = len(self.cmd_history)
+        stack = []
         for cmd in cmd_line:
             self.cmd_history += cmd
             if cmd=='[':
@@ -117,9 +137,20 @@ class BrainFuck:
                 self._jump[pos] = stack.pop()
                 self._jump[self._jump[pos]] = pos
             pos+=1
+
+        #Execute commands, keeping track of #commands executed
+        rec_depth = 0
         while self._cmd_pointer<len(self.cmd_history):
-            cmds[self.cmd_history[self._cmd_pointer]]()
-            self._cmd_pointer += 1
+            rec_depth += 1
+            if rec_depth < MAX_RECURSION:
+                cmds[self.cmd_history[self._cmd_pointer]]()
+                self._cmd_pointer += 1
+            else:
+                print('MAX recursion reached!')
+                self.cmd_history = backup_history
+                self._cmd_pointer = len(backup_history)
+                self.cells = backup_cells
+                break
 
     def interpreter(self):
         while True:
@@ -127,11 +158,9 @@ class BrainFuck:
             while not self.is_balanced(cmd_line):
                 tmp = input('.. ')
                 cmd_line = cmd_line+tmp if tmp else 'skip'
-            if cmd_line=='help':
-                print(help_text)
+            if cmd_line=='help': print(help_text)
             elif not cmd_line: break
-            else:
-                self.execute(cmd_line)
+            else: self.execute(cmd_line)
 
 class Cells(dict):
     def __init__(self, **kwargs):
@@ -143,10 +172,20 @@ class Cells(dict):
             return super(Cells, self).__getitem__(key)
         return 0
 
+    def __setitem__(self, key, value):
+        super(Cells, self).__setitem__(key, value)
+        if not value: del self[key]
+
+    def backup(self):
+        cls = self.__class__
+        new_cells = cls.__new__(cls)
+        for key, value in self.items():
+            new_cells[key] = value
+        return new_cells
+
     def print_pos(self, pos):
-        k = [v for v in self.keys()]+[pos]
-        m = min(k)
-        M = max(k)
+        if self: m, M = min(min(self), pos), max(max(self), pos)
+        else: m = M = pos
         print_list = [0 for _ in range(m, M+1)]
         for key,value in self.items():
             print_list[key-m] = value
